896ddd7e48f0d1e9176e7b86ac59d781095580f4
Ch. Michalakis <mpampisb@gmail.com>
2025-12-15 13:34:33 +0200

    feat: annotate OrderService class as transactional

--- order-service/src/main/java/com/babis/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/babis/microservices/order/service/OrderService.java
@@ -6,11 +6,13 @@ import com.babis.microservices.order.model.Order;
 import com.babis.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
