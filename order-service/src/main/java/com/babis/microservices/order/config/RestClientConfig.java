0bfd4bf25383965341c52c9ce82b96b16902323c
Ch. Michalakis <mpampisb@gmail.com>
2025-12-29 21:34:16 +0200

    docs: add explanatory comments in RestClientConfig class

--- order-service/src/main/java/com/babis/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/babis/microservices/order/config/RestClientConfig.java
@@ -14,15 +14,27 @@ public class RestClientConfig {
     @Value("${inventory.url}")
     private String inventoryServiceBaseUrl;
 
+    /**
+     * Creating the InventoryClient Bean, a runtime implementation of InventoryClient interface
+     * using Spring's HTTP Interface Proxy mechanism.
+     * Proxy -> An object that pretends to be another object and intercepts method calls.
+     */
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceBaseUrl)
                 .build();
 
+        // Adapting RestClient to Spring’s Proxy System | bridge RestClient with the HTTP Interface Proxy
+        // Spring’s proxy system is client-agnostic, so we need to adapt RestClient to work with it. We could use
+        // other HTTP clients as well, like WebClient or Apache HttpClient, by using their respective adapters.
         var restClientAdapter = RestClientAdapter.create(restClient);
+
+        // the HttpServiceProxyFactory is responsible for creating proxy instances of HTTP service interfaces.
+        // It uses the provided RestClientAdapter to handle the actual HTTP communication.
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 
+        // Creates a class at runtime that implements the InventoryClient interface
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
 
