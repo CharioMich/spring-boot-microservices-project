ace4f8b9e41f1c78f4615615df695fc37d5ad290
Ch. Michalakis <mpampisb@gmail.com>
2026-01-24 20:31:20 +0200

    feat(frontend): integrate Angular app into the repo

---
+++ frontend/src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,46 @@
+// import {HttpInterceptorFn} from "@angular/common/http";
+// import {inject} from "@angular/core";
+// import {OidcSecurityService} from "angular-auth-oidc-client";
+//
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   const authService = inject(OidcSecurityService);
+//
+//   authService.getAccessToken().subscribe(token => {
+//     if (token) {
+//       let header = 'Bearer ' + token;
+//       let headers = req.headers
+//         .set('Authorization', header);
+//
+//       req = req.clone({headers});
+//
+//       return next(req);
+//     }
+//
+//     return next(req);
+//   })
+//
+//   return next(req);
+//
+// }
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { from, switchMap } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(OidcSecurityService);
+
+  return from(authService.getAccessToken()).pipe(
+    switchMap(token => {
+      if (token) {
+        const authReq = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        return next(authReq);
+      }
+      return next(req);
+    })
+  );
+};
